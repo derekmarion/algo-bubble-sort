4bb80ec37a4bb8813efdd4b14feed34ffaf24888
Derek Marion <derek.j.marion@gmail.com>
2023-10-16 13:45:48 -0700

    Added tests, determined that algo is O(n^2) complexity class

--- python/test_bubble_sort.py
+++ python/test_bubble_sort.py
@@ -7,3 +7,15 @@ def test_01():
 def test_02():
     sequence = [1, 0, 2, 3, 4, 5]
     assert Solution.bubble_sort(sequence) == 1
+
+def test_03():
+    sequence = [5, 4, 3, 2, 1]
+    assert Solution.bubble_sort(sequence) == 10
+
+def test_04():
+    sequence = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    assert Solution.bubble_sort(sequence) == 45
+
+def test_05():
+    sequence = list(range(20, 0, -1))
+    assert Solution.bubble_sort(sequence) == 190
