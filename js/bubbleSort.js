8c5d82bb1108c222421e7b365b3063aa647d91b9
derekmarion <derek.j.marion@gmail.com>
2023-10-16 12:52:32 -0700

    Initial commit

---
+++ js/bubbleSort.js
@@ -0,0 +1,7 @@
+var sequence = [4, 3, 5, 0, 1]
+var swaps = 0
+
+// Your Code Here
+
+console.log("Final result: " + result)
+console.log("Swaps: " + swaps)
---
+++ python/bubble_sort.py
@@ -0,0 +1,7 @@
+sequence = [4, 3, 5, 0, 1]
+swaps = 0
+
+# Your Code Here
+
+print(f"Final result: {result}")
+print(f"Swaps: {swaps}")
