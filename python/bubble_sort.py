15412de14d128a8e58863e98ebfa479a663b4716
Derek Marion <derek.j.marion@gmail.com>
2023-10-16 13:27:45 -0700

    Write algorithm of efficiency class O(n)

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,19 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
-
-# Your Code Here
-
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+class Solution:
+    def bubble_sort(array: list) -> int:
+        swaps = 0
+        idx = 0
+        while idx < len(array):
+            previous = array[idx]
+            try:
+                current = array[idx+1]
+            except IndexError:
+                print(f"Final result: {array}")
+                print(f"Swaps: {swaps}")
+                return swaps
+            if current < previous:
+                array[idx+1] = previous
+                array[idx] = current
+                idx = 0
+                swaps += 1
+            else:
+                idx += 1
